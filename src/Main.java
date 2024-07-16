059362a63630715cbca1a02a44d87348d9c32684
João Aurélio <joao_mcd@hotmail.com>
2024-07-16 17:34:54 -0300

    teste

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
+		venilton.setNome("Venilton");		
 		Conta cc = new ContaCorrente(venilton);
 		Conta poupanca = new ContaPoupanca(venilton);
+		Cliente joao = new Cliente();
+		venilton.setNome("Joao");		
+		Conta cc2 = new ContaCorrente(joao);
+		Conta poupanca2 = new ContaPoupanca(joao);
 
+		
 		cc.depositar(100);
 		cc.transferir(100, poupanca);
 		
